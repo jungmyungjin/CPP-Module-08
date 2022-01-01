c8c4b0597eb34810f6ad81fee1b1f8b26025138d
jungmyungjin <36308113+jungmyungjin@users.noreply.github.com>
2022-01-01 17:13:49 +0900

    제출전 수정

--- ex02/mutantstack.hpp
+++ ex02/mutantstack.hpp
@@ -1,34 +1,49 @@
-#ifndef MUTANT_STACK_HPP
-# define MUTANT_STACK_HPP
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mutantstack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mjung <mjung@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/01 16:42:20 by mjung             #+#    #+#             */
+/*   Updated: 2022/01/01 17:13:28 by mjung            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include <iostream>
-# include <stack>
+#ifndef MUTANT_STACK_HPP
+#define MUTANT_STACK_HPP
+
+#include <iostream>
+#include <stack>
+#include <deque>
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-//	https://stackoverflow.com/questions/525365/does-stdstack-expose-iterators
 public:
-	using iterator = typename std::deque<T>::iterator;
-	using const_iterator = typename std::deque<T>::const_iterator;
-	using reverse_iterator = typename std::deque<T>::reverse_iterator;
-	using const_reverse_iterator = typename std::deque<T>::const_reverse_iterator;
+	MutantStack(void) {};
+	virtual ~MutantStack(void) {};
+	MutantStack& operator=(const MutantStack& target) {*this = target; return *this;};
+	MutantStack(const MutantStack& target) {*this = target;};
 
-	 // Container https://en.cppreference.com/w/cpp/container
-	iterator							begin(void) { return (this->c.begin()); }
-	iterator							end(void)			{ return (this->c.end()); }
-	const_iterator						cbegin(void) const	{ return (this->c.cbegin()); }
-	const_iterator						cend(void) const	{ return (this->c.cend()); }
-	reverse_iterator					rbegin(void)		{ return (this->c.rbegin()); }
-	reverse_iterator					rend(void)			{ return (this->c.rend()); }
-	const_reverse_iterator				crbegin(void) const	{ return (this->c.crbegin()); }
-	const_reverse_iterator				crend(void) const	{ return (this->c.crend()); }
+	typedef typename std::stack<T>::container_type::iterator iterator;
+	typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-	MutantStack&						operator=(const MutantStack& m) = default;
-	MutantStack(void) = default;
-	MutantStack(const MutantStack& m) = default;
-	MutantStack(MutantStack&& m) = default;
-	~MutantStack(void) = default;
+	// 컨테이너 멤버 함수 테이블
+	// > https://en.cppreference.com/w/cpp/container
+	// 참고자료
+	// > https://stackoverflow.com/questions/525365/does-stdstack-expose-iterators
+	iterator							begin(void) { return (this->c.begin()); };
+	iterator							end(void)			{ return (this->c.end()); };
+	const_iterator						cbegin(void) const	{ return (this->c.cbegin()); };
+	const_iterator						cend(void) const	{ return (this->c.cend()); };
+	reverse_iterator					rbegin(void)		{ return (this->c.rbegin()); };
+	reverse_iterator					rend(void)			{ return (this->c.rend()); };
+	const_reverse_iterator				crbegin(void) const	{ return (this->c.crbegin()); };
+	const_reverse_iterator				crend(void) const	{ return (this->c.crend()); };
+
 };
 
 #endif
